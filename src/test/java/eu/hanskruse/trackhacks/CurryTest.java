5ee3144dc507d46d841abc720ff63418776e69a0
Hans Kruse <Hans.Kruse@InnoValor.nl>
2016-11-22 09:08:53 +0100

    Added forgotten testclass.

---
+++ src/test/java/eu/hanskruse/trackhacks/CurryTest.java
@@ -0,0 +1,37 @@
+package eu.hanskruse.trackhacks;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import eu.hanskruse.trackhacks.noaber.$;
+import junit.framework.TestCase;
+
+public class CurryTest extends TestCase implements $ {
+
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public CurryTest(String testName) {
+		super(testName);
+	}
+	
+	public void testLeftCurryOnBiFunction(){
+	 final String expected ="42";
+	 BiFunction<Integer,Boolean, String> f = (x,b) -> Integer.toString((b?x:42));	
+	 Function<Integer, Function<Boolean, String>> cf=leftCurry(f);
+	 final String actual =cf.apply(3).apply(false);
+	 assertEquals(expected, actual);
+	 
+	}
+	
+	public void testRightCurryOnBiFunction(){
+	  final String expected ="42";
+	  BiFunction<Integer,Boolean, String> f = (x,b) -> Integer.toString((b?x:42));	
+	  Function<Boolean, Function<Integer, String>> cf=rightCurry(f);
+	  String actual = cf.apply(false).apply(3);
+	  assertEquals(expected, actual);
+	}
+}
