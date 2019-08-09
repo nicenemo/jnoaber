f39e754a234654af6a75d184fdc76faf7f3c79ce
Hans Kruse <kruse@hanskruse.eu>
2019-08-09 17:55:25 +0200

    removed for comprehension for streams, it cannot work correctly

--- src/main/java/eu/hanskruse/trackhacks/noaber/WithStream.java
+++ src/main/java/eu/hanskruse/trackhacks/noaber/WithStream.java
@@ -1,14 +1,10 @@
 package eu.hanskruse.trackhacks.noaber;
 
 import java.util.Arrays;
-import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
-import java.util.function.Supplier;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
-import java.util.AbstractMap.SimpleEntry;
 
 /**
  * Stream functionality.
@@ -59,44 +55,4 @@ public interface WithStream {
   default <T> Stream<T> stream(final T... items) {
     return Arrays.stream(items);
   }
-
-  /**
-   * Combine two streams. Similar to Scala's for comprehension or .NET LINQ's from
-   * clause. We use {@code Supplier}s because a {@code Stream} can be consumed
-   * only once.
-   *
-   * @param <T0> type of the 0th stream
-   * @param <T1> type of the 1th stream
-   * @param s0   the 0th stream supplier
-   * @param s1   the 1th stream supplier
-   * @return combined stream
-   */
-  default <T0, T1> Stream<Tuple> stream(final Supplier<Stream<T0>> s0, final Supplier<Stream<T1>> s1) {
-    return s0.get().flatMap(t0 -> s1.get().map(t1 -> Tuple.of(t0, t1)));
-  }
-/*
-  default <//
-      T0, //
-      T1, //
-      T2> //
-  Stream<Tuple> stream(//
-      final Supplier<Stream<T1>> s0, //
-      final Supplier<Stream<T1>> s1, //
-      final Supplier<Stream<T2>> s2) {
-    return s0.get().flatMap(t0 -> s1.get().flatMap(t1 -> s2.get().map(t2 -> Tuple.of(t0, t1, t2))));
-      }*/
-/*
-  default <//
-      T0, //
-      T1, //
-      T2, //
-      T3> //
-  Stream<Tuple> stream(//
-      final Supplier<Stream<T1>> s0, //
-      final Supplier<Stream<T1>> s1, //
-      final Supplier<Stream<T2>> s2, //
-      final Supplier<Stream<T3>> s3) {
-    return s0.get()
-        .flatMap(t0 -> s1.get().flatMap(t1 -> s2.get().flatMap(t2 -> s3.get().map(t3 -> Tuple.of(t0, t1, t2, t3)))));
-  }*/
 }
