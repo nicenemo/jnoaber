2e071620f03500fad519f0dae58d557cba3f16fb
Hans Kruse <Hans.Kruse@innovalor.nl>
2019-09-15 13:20:11 +0200

    rename WithPredicate to WithFunction

--- src/main/java/eu/hanskruse/trackhacks/noaber/with/WithFunction.java
+++ src/main/java/eu/hanskruse/trackhacks/noaber/with/WithFunction.java
@@ -8,7 +8,7 @@ import java.util.function.Function;
 /**
  *  Predicate Support.
  */
-public interface WithPredicate {
+public interface WithFunction {
 
   /**
    * Right apply to turn a {@code BiPredicate} into a {@code Predicate}.
@@ -25,13 +25,13 @@ public interface WithPredicate {
   default <T1, T2> Function<T1,Predicate<T2>> leftApply(final BiPredicate<T1, T2> p) {
     return t1 -> t2 -> p.test(t1, t2);
   }
-
-  default <T1,T2,R> Function<T2,Function<T1,R>> rightApply(final BiFunction<T1,T2,R> f){
+/*
+  default <T1,T2,R> Function<? extends T2, Function<? extends T1,? super R>> rightApply(final BiFunction<? extends T1,? extends T2,? super R> f){
     return (T2 t2) -> (T1 t1) -> f.apply(t1,t2);
-  }
+  }*/
 
-  default <T1,T2,R> Function<T1,Function<T2,R>> leftApply(final BiFunction<T1,T2,R> f){
-    return (T1 t1) -> (T2 t2) -> f.apply(t1,t2);
+  default <T1,T2,R> Function<T1, Function<T2, R>> leftApply(final BiFunction<T1,T2, R> f){
+     return t1 -> t2 -> f.apply(t1,t2);
   }
 
 
